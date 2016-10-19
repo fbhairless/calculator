5305338114db66237c5348c13650f605979ab43c
Hal Fairless and James Lee <hal@>
2016-10-19 11:47:15 -0700

    Add additional math operations for calculator app.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,45 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
